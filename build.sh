e686bb99c3d71485324b1411049985f0cdabe1fc
allkern <lisandroaalarcon@gmail.com>
2025-04-04 14:54:26 -0300

    macos: Keep trying

--- build.sh
+++ build.sh
@@ -7,19 +7,19 @@ make USE_INTRINSICS=1 TARGET=x86_64-apple-macos10.15 -j8
 make USE_INTRINSICS=0 TARGET=arm64-apple-macos10.15 -j8
 
 # Make universal binary
-lipo -create -output ./bin/iris ./bin/iris_x86_64-apple-macos10.15 ./bin/iris_arm64-apple-macos10.15
+lipo -create -output ./bin/iris "./bin/iris_x86_64-apple-macos10.15" "./bin/iris_arm64-apple-macos10.15"
 
 # Create bundle filesystem
-mkdir -p iris.app/Contents/MacOS/Libraries
+mkdir -p ./dist/iris.app/Contents/MacOS/Libraries
 
 # Move executable to folder
-mv bin/iris iris.app/Contents/MacOS
+mv ./bin/iris ./dist/iris.app/Contents/MacOS
 
 # Make executable
-chmod 777 iris.app/Contents/MacOS/iris
+chmod 777 ./dist/iris.app/Contents/MacOS/iris
 
 # Bundle required dylibs
-dylibbundler -b -x ./iris.app/Contents/MacOS/iris -d ./iris.app/Contents/Libraries/ -p @executable_path/../Libraries/ -cd
+dylibbundler -b -x ./dist/iris.app/Contents/MacOS/iris -d ./dist/iris.app/Contents/Libraries/ -p @executable_path/../Libraries/ -cd
 
 # Move plist to Contents folder
-mv Info.plist iris.app/Contents/Info.plist
+mv Info.plist ./dist/iris.app/Contents/Info.plist
