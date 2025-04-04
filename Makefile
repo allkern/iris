b2b8448a9c323d23de8c53b0d14a66959bbab694
allkern <lisandroaalarcon@gmail.com>
2025-04-04 15:15:13 -0300

    build: Fix Makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,6 @@
 # We need a different Makefile for macOS
 PLATFORM := $(shell uname -s)
 
-$(shell echo $(PLATFORM))
-
 ifeq ($(PLATFORM),Darwin)
 include Makefile.macos
 else
--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 # Build emulator
 make USE_INTRINSICS=1 TARGET=x86_64-apple-macos10.15 -j8
-make USE_INTRINSICS=0 TARGET=arm64-apple-macos10.15 -j8
+# make USE_INTRINSICS=0 TARGET=arm64-apple-macos10.15 -j8
 
 # Make universal binary
 lipo -create -output ./bin/iris "./bin/iris_x86_64-apple-macos10.15" "./bin/iris_arm64-apple-macos10.15"
