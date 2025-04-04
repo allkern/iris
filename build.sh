3bf57a8ae475f553d6aee1a61adff1ebed79bd09
allkern <lisandroaalarcon@gmail.com>
2025-04-04 14:27:00 -0300

    macos: Generate universal binaries

--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@
 ./setup_gl3w.sh
 
 # Build emulator
-make USE_INTRINSICS=0 -j8
+make USE_INTRINSICS=1 TARGET=x86_64-apple-macos10.15 -j8
+make USE_INTRINSICS=0 TARGET=arm64-apple-macos10.15 -j8
+
+# Make universal binary
+lipo -create -output ./bin/iris ./bin/iris.x86_64-apple-macos10.15 ./bin/iris.arm64-apple-macos10.15
 
 # Create bundle filesystem
 mkdir -p iris.app/Contents/MacOS/Libraries
