ccf01ee6dc5aed91bd110e472531e6756f0f1f91
allkern <lisandroaalarcon@gmail.com>
2025-04-04 19:11:05 -0300

    test: Testing

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 ./setup_gl3w.sh
 
+ls /usr/local/
+ls /usr/local/sdl2
+ls /usr/local/sdl2/2.32.4/
+ls /usr/local/sdl2/2.32.4/lib/
+
 # Build emulator
 make USE_INTRINSICS=1 TARGET=x86_64-apple-macos10.15 -j8
 # make USE_INTRINSICS=0 TARGET=arm64-apple-macos10.15 -j8
