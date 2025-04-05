9198d4d0ab3f36044928c38e12abc03e12f3287e
allkern <lisandroaalarcon@gmail.com>
2025-04-05 10:37:12 -0300

    macos: Remove font building

--- build.sh
+++ build.sh
@@ -3,11 +3,7 @@
 ./setup_gl3w.sh
 
 # Build emulator
-make USE_INTRINSICS=1 TARGET=x86_64-apple-macos10.15 -j8
-# make USE_INTRINSICS=0 TARGET=arm64-apple-macos10.15 -j8
-
-# Make universal binary
-lipo -create -output ./bin/iris "./bin/iris_x86_64-apple-macos10.15" "./bin/iris_arm64-apple-macos10.15"
+make USE_INTRINSICS=0 -j8
 
 # Create bundle filesystem
 mkdir -p ./dist/iris.app/Contents/MacOS/Libraries
