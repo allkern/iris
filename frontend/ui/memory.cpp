12d39118aeb15f0b092332c99531407117fa7e18
allkern <lisandroaalarcon@gmail.com>
2025-04-25 12:17:45 -0300

    ui: Add various regions to memory viewer

--- frontend/ui/memory.cpp
+++ frontend/ui/memory.cpp
@@ -17,11 +17,109 @@ void show_memory_viewer(iris::instance* iris) {
     struct ps2_state* ps2 = iris->ps2;
 
     if (Begin("Memory", &iris->show_memory_viewer)) {
-        PushFont(iris->font_code);
+        if (BeginTabBar("##tabbar")) {
+            if (BeginTabItem("EE")) {
+                PushFont(iris->font_code);
 
-        editor.DrawContents(ps2->ee_ram->buf, RAM_SIZE_32MB, 0);
+                editor.DrawContents(ps2->ee_ram->buf, RAM_SIZE_32MB, 0);
 
-        PopFont();
+                PopFont();
+
+                EndTabItem();
+            }
+
+            if (BeginTabItem("EE SPR")) {
+                PushFont(iris->font_code);
+
+                editor.DrawContents(ps2->ee->scratchpad->buf, 0x4000, 0);
+
+                PopFont();
+
+                EndTabItem();
+            }
+
+            if (BeginTabItem("IOP")) {
+                PushFont(iris->font_code);
+
+                editor.DrawContents(ps2->iop_ram->buf, RAM_SIZE_2MB, 0);
+
+                PopFont();
+
+                EndTabItem();
+            }
+
+            if (BeginTabItem("IOP SPR")) {
+                PushFont(iris->font_code);
+
+                editor.DrawContents(ps2->iop_spr->buf, RAM_SIZE_1KB, 0);
+
+                PopFont();
+
+                EndTabItem();
+            }
+
+            if (BeginTabItem("VRAM")) {
+                PushFont(iris->font_code);
+
+                editor.DrawContents(ps2->gs->vram, 0x400000, 0);
+
+                PopFont();
+
+                EndTabItem();
+            }
+
+            if (BeginTabItem("SPU2")) {
+                PushFont(iris->font_code);
+
+                editor.DrawContents(ps2->spu2->ram, RAM_SIZE_2MB, 0);
+
+                PopFont();
+
+                EndTabItem();
+            }
+
+            if (BeginTabItem("VU0 IMEM")) {
+                PushFont(iris->font_code);
+
+                editor.DrawContents(ps2->vu0->micro_mem, 0x1000, 0);
+
+                PopFont();
+
+                EndTabItem();
+            }
+
+            if (BeginTabItem("VU0 DMEM")) {
+                PushFont(iris->font_code);
+
+                editor.DrawContents(ps2->spu2->ram, 0x1000, 0);
+
+                PopFont();
+
+                EndTabItem();
+            }
+
+            if (BeginTabItem("VU1 IMEM")) {
+                PushFont(iris->font_code);
+
+                editor.DrawContents(ps2->spu2->ram, 0x4000, 0);
+
+                PopFont();
+
+                EndTabItem();
+            }
+
+            if (BeginTabItem("VU1 DMEM")) {
+                PushFont(iris->font_code);
+
+                editor.DrawContents(ps2->spu2->ram, 0x4000, 0);
+
+                PopFont();
+
+                EndTabItem();
+            }
+
+            EndTabBar();
+        }
     } End();
 }
 
