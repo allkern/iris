d8991ecfdd9d9a08e29d9162e7946b3c472983a9
allkern <lisandroaalarcon@gmail.com>
2025-04-25 12:34:18 -0300

    dma: CDVD wait for DMA ready dma: Better SIO2_In/Out stubs

--- src/iop/dma.c
+++ src/iop/dma.c
@@ -59,7 +59,7 @@ struct ps2_iop_dma* ps2_iop_dma_create(void) {
     return malloc(sizeof(struct ps2_iop_dma));
 }
 
-void ps2_iop_dma_init(struct ps2_iop_dma* dma, struct ps2_iop_intc* intc, struct ps2_sif* sif, struct ps2_cdvd* cdvd, struct ps2_dmac* ee_dma, struct sched_state* sched, struct iop_bus* bus) {
+void ps2_iop_dma_init(struct ps2_iop_dma* dma, struct ps2_iop_intc* intc, struct ps2_sif* sif, struct ps2_cdvd* cdvd, struct ps2_dmac* ee_dma, struct ps2_sio2* sio2, struct sched_state* sched, struct iop_bus* bus) {
     memset(dma, 0, sizeof(struct ps2_iop_dma));
 
     dma->intc = intc;
@@ -68,6 +68,7 @@ void ps2_iop_dma_init(struct ps2_iop_dma* dma, struct ps2_iop_intc* intc, struct
     dma->drive = cdvd;
     dma->sched = sched;
     dma->ee_dma = ee_dma;
+    dma->sio2 = sio2;
 
     dma->dmacinten = 0x01;
 }
@@ -146,9 +147,9 @@ void iop_dma_handle_cdvd_transfer(struct ps2_iop_dma* dma) {
 
     // Channel not yet started
     if (!(dma->cdvd.chcr & 0x1000000)) {
-        printf("iop: CDVD transfer incoming, channel not yet started\n");
+        printf("iop: CDVD transfer incoming, channel not yet started (%08x)\n", dma->cdvd.chcr);
 
-        exit(1);
+        // exit(1);
 
         return;
     }
@@ -157,8 +158,19 @@ void iop_dma_handle_cdvd_transfer(struct ps2_iop_dma* dma) {
 
     // uint32_t addr = dma->cdvd.madr;
 
-    for (int i = 0; i < dma->drive->buf_size; i++)
-        iop_bus_write8(dma->bus, dma->cdvd.madr++, dma->drive->buf[i]);
+    int i = 0;
+
+    while (dma->cdvd.transfer_size && dma->drive->buf_size) {
+        iop_bus_write8(dma->bus, dma->cdvd.madr++, dma->drive->buf[i++]);
+
+        dma->drive->buf_size--;
+        dma->cdvd.transfer_size--;
+    }
+
+    // printf("dma: buf_size=%d transfer_size=%d\n",
+    //     dma->drive->buf_size,
+    //     dma->cdvd.transfer_size
+    // );
 
     // int size = dma->drive->buf_size;
 
@@ -183,16 +195,16 @@ void iop_dma_handle_cdvd_transfer(struct ps2_iop_dma* dma) {
     //     size -= 16;
     // }
 
-    dma->drive->buf_size = 0;
-
     // Only end the transfer when there aren't any
-    // sectors left to copy
-    if (dma->drive->read_count)
+    // blocks left to copy
+    if (dma->cdvd.transfer_size)
         return;
 
+    // printf("dma: Sending IRQ to IOP\n");
     iop_dma_set_dicr_flag(dma, IOP_DMA_CDVD);
     iop_dma_check_irq(dma);
 
+    // printf("cdvd: Ending transfer\n");
     dma->cdvd.chcr &= ~0x1000000;
     dma->cdvd.bcr = 0;
 }
@@ -389,24 +401,29 @@ void iop_dma_handle_sif1_transfer(struct ps2_iop_dma* dma) {
     dma->sif1.chcr &= ~0x1000000;
 }
 void iop_dma_handle_sio2_in_transfer(struct ps2_iop_dma* dma) {
-    printf("iop: SIO2 in channel unimplemented\n");
+    uint32_t size = (dma->sio2_in.bcr & 0xffff) * (dma->sio2_in.bcr >> 16);
 
-    // uint32_t size = (dma->sio2_in.bcr & 0xffff) * (dma->sio2_in.bcr >> 16);
+    // printf("dma: SIO2 in transfer size=%d\n", size);
+
+    // sio2_dma_reset(dma->sio2);
 
     // for (int i = 0; i < size; i++) {
     //     uint32_t w = iop_bus_read32(dma->bus, dma->sio2_in.madr);
 
-    //     printf("w=%08x\n", w);
-    //     // iop_bus_write32(dma->bus, 0x)
+    //     queue_push(dma->sio2->in, (w >> 0) & 0xff);
+    //     queue_push(dma->sio2->in, (w >> 8) & 0xff);
+    //     queue_push(dma->sio2->in, (w >> 16) & 0xff);
+    //     queue_push(dma->sio2->in, (w >> 24) & 0xff);
+
+    //     printf("%02x %02x %02x %02x\n",
+    //         (w >> 0) & 0xff,
+    //         (w >> 8) & 0xff,
+    //         (w >> 16) & 0xff,
+    //         (w >> 24) & 0xff
+    //     );
+
     //     dma->sio2_in.madr += 4;
     // }
-
-    // printf("chcr=%08x madr=%08x bcr=%08x tadr=%08x\n",
-    //     dma->sio2_in.chcr,
-    //     dma->sio2_in.madr,
-    //     dma->sio2_in.bcr,
-    //     dma->sio2_in.tadr
-    // );
 
     iop_dma_set_dicr_flag(dma, IOP_DMA_SIO2_IN);
     iop_dma_check_irq(dma);
@@ -414,19 +431,36 @@ void iop_dma_handle_sio2_in_transfer(struct ps2_iop_dma* dma) {
     dma->sio2_in.chcr &= ~0x1000000;
 }
 void iop_dma_handle_sio2_out_transfer(struct ps2_iop_dma* dma) {
-    printf("iop: SIO2 out channel unimplemented\n");
+    // if (queue_is_empty(dma->sio2->out)) {
+    //     // printf("dma: Waiting for SIO2 out\n");
 
-    iop_dma_set_dicr_flag(dma, IOP_DMA_SIO2_OUT);
-    iop_dma_check_irq(dma);
+    //     return;
+    // }
 
-    // Clear memory
+    // // printf("dma: Doing SIO2 out transfer size=%d bcr=%08x\n", queue_size(dma->sio2->out), dma->sio2_out.bcr);
+
+    // iop_dma_set_dicr_flag(dma, IOP_DMA_SIO2_OUT);
+    // iop_dma_check_irq(dma);
+
+    // for (int b = 0; b < (dma->sio2_out.bcr >> 16); b++) {
+    //     for (int i = 0; i < (dma->sio2_out.bcr & 0xffff); i++) {
+    //         iop_bus_write8(dma->bus, dma->sio2_out.madr++, queue_pop(dma->sio2->out));
+    //         iop_bus_write8(dma->bus, dma->sio2_out.madr++, queue_pop(dma->sio2->out));
+    //         iop_bus_write8(dma->bus, dma->sio2_out.madr++, queue_pop(dma->sio2->out));
+    //         iop_bus_write8(dma->bus, dma->sio2_out.madr++, queue_pop(dma->sio2->out));
+    //     }
+    // }
     for (int b = 0; b < (dma->sio2_out.bcr >> 16); b++) {
         for (int i = 0; i < (dma->sio2_out.bcr & 0xffff); i++) {
-            iop_bus_write8(dma->bus, dma->sio2_out.madr++, 0xff);
+            iop_bus_write8(dma->bus, dma->sio2_out.madr++, 0);
+            iop_bus_write8(dma->bus, dma->sio2_out.madr++, 0);
+            iop_bus_write8(dma->bus, dma->sio2_out.madr++, 0);
+            iop_bus_write8(dma->bus, dma->sio2_out.madr++, 0);
         }
     }
 
-    printf("chcr=%08x madr=%08x bcr=%08x tadr=%08x\n", dma->sio2_out.chcr, dma->sio2_out.madr, dma->sio2_out.bcr, dma->sio2_out.tadr);
+    iop_dma_set_dicr_flag(dma, IOP_DMA_SIO2_OUT);
+    iop_dma_check_irq(dma);
 
     dma->sio2_out.chcr &= ~0x1000000;
 }
@@ -470,12 +504,23 @@ void ps2_iop_dma_write32(struct ps2_iop_dma* dma, uint32_t addr, uint64_t data) 
         switch (addr & 0xf) {
             case 0x0: c->madr = data; return;
             case 0x4: c->bcr = data; return;
-            case 0x6: c->bcr &= 0xffff; c->bcr |= data << 16; return;
             case 0x8: {
                 c->chcr = data;
 
-                if (!(c->chcr & 0x1000000))
+                if (!(c->chcr & 0x1000000)) {
                     return;
+                }
+
+                c->transfer_size = (c->bcr >> 16) * ((c->bcr & 0xffff) * 4);
+
+                // if ((addr & 0xff0) == 0x0b0) {
+                //     FILE* file = fopen("cdvd.dump", "a");
+                //     fprintf(file, "iop: Starting %s channel with chcr=%08x madr=%08x bcr=%08x tadr=%08x\n", iop_dma_get_channel_name(addr), data, c->madr, c->bcr, c->tadr);
+                //     fclose(file);
+                // }
+
+                // if ((addr & 0xff0) == 0x0b0)
+                // printf("iop: Starting %s channel with chcr=%08x madr=%08x bcr=%08x tadr=%08x\n", iop_dma_get_channel_name(addr), data, c->madr, c->bcr, c->tadr);
 
                 // printf("iop: Starting %s channel with chcr=%08x madr=%08x bcr=%08x tadr=%08x\n", iop_dma_get_channel_name(addr), data, c->madr, c->bcr, c->tadr);
 
@@ -526,4 +571,59 @@ void ps2_iop_dma_write32(struct ps2_iop_dma* dma, uint32_t addr, uint64_t data) 
     }
 
     printf("iop_dma: Unknown DMA register write %08x %08lx\n", addr, data);
+}
+
+uint64_t ps2_iop_dma_read16(struct ps2_iop_dma* dma, uint32_t addr) {
+    struct iop_dma_channel* c = iop_dma_get_channel(dma, addr);
+
+    if (c) {
+        switch (addr & 0xf) {
+            case 0x0: return c->madr;
+            case 0x4: return c->bcr;
+            case 0x8: return c->chcr;
+            case 0xc: return c->tadr;
+        }
+
+        const char* name = iop_dma_get_channel_name(addr);
+
+        printf("iop_dma: Unknown %s register read %08x\n", name, addr);
+
+        return 0;
+    }
+
+    switch (addr) {
+        case 0x1f8010f0: return dma->dpcr;
+        case 0x1f801570: return dma->dpcr2;
+        case 0x1f8010f4: return dma->dicr;
+        case 0x1f801574: return dma->dicr2;
+        case 0x1f801578: return dma->dmacen;
+        case 0x1f80157c: return dma->dmacinten;
+    }
+
+    printf("iop_dma: Unknown DMA register read %08x\n", addr);
+
+    return 0;
+}
+
+void ps2_iop_dma_write16(struct ps2_iop_dma* dma, uint32_t addr, uint64_t data) {
+    struct iop_dma_channel* c = iop_dma_get_channel(dma, addr);
+
+    if (c) {
+        switch (addr & 0xf) {
+            case 0x4: if ((addr & 0xff0) == 0x0b0) printf("iop: bcrlo write16 %08x\n", data); c->bcr &= ~0xffff; c->bcr |= data; return;
+            case 0x6: if ((addr & 0xff0) == 0x0b0) printf("iop: bcrhi write16 %08x\n", data); c->bcr &= 0xffff; c->bcr |= data << 16; return;
+        }
+
+        const char* name = iop_dma_get_channel_name(addr);
+
+        printf("iop_dma: Unknown 16-bit %s register write %08x %08lx\n", name, addr, data);
+
+        exit(1);
+
+        return;
+    }
+
+    printf("iop_dma: Unknown DMA register write %08x %08lx\n", addr, data);
+
+    exit(1);
 }
--- src/iop/dma.h
+++ src/iop/dma.h
@@ -14,6 +14,7 @@ extern "C" {
 #include "intc.h"
 #include "cdvd.h"
 #include "sched.h"
+#include "sio2.h"
 
 #include "bus_decl.h"
 
@@ -47,6 +48,7 @@ struct iop_dma_channel {
     int irq;
     int eot;
     int extra;
+    uint32_t transfer_size;
 };
 
 struct ps2_iop_dma {
@@ -77,12 +79,15 @@ struct ps2_iop_dma {
     struct ps2_sif* sif;
     struct ps2_cdvd* drive;
     struct ps2_dmac* ee_dma;
+    struct ps2_sio2* sio2;
     struct sched_state* sched;
 };
 
 struct ps2_iop_dma* ps2_iop_dma_create(void);
-void ps2_iop_dma_init(struct ps2_iop_dma* dma, struct ps2_iop_intc* intc, struct ps2_sif* sif, struct ps2_cdvd* cdvd, struct ps2_dmac* ee_dma, struct sched_state* sched, struct iop_bus* bus);
+void ps2_iop_dma_init(struct ps2_iop_dma* dma, struct ps2_iop_intc* intc, struct ps2_sif* sif, struct ps2_cdvd* cdvd, struct ps2_dmac* ee_dma, struct ps2_sio2* sio2, struct sched_state* sched, struct iop_bus* bus);
 void ps2_iop_dma_destroy(struct ps2_iop_dma* dma);
+uint64_t ps2_iop_dma_read16(struct ps2_iop_dma* dma, uint32_t addr);
+void ps2_iop_dma_write16(struct ps2_iop_dma* dma, uint32_t addr, uint64_t data);
 uint64_t ps2_iop_dma_read32(struct ps2_iop_dma* dma, uint32_t addr);
 void ps2_iop_dma_write32(struct ps2_iop_dma* dma, uint32_t addr, uint64_t data);
 void iop_dma_handle_mdec_in_transfer(struct ps2_iop_dma* dma);
