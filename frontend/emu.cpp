7f2cf6f623d68f262ecfbfc1258565652bcde785
allkern <lisandroaalarcon@gmail.com>
2025-12-11 16:34:50 -0300

    mcd: Implement PS1/PS2 card detection

--- frontend/emu.cpp
+++ frontend/emu.cpp
@@ -18,4 +18,38 @@ void destroy(iris::instance* iris) {
     ps2_destroy(iris->ps2);
 }
 
+int attach_memory_card(iris::instance* iris, int slot, const char* path) {
+    detach_memory_card(iris, slot);
+
+    FILE* file = fopen(path, "rb");
+
+    if (!file) {
+        return 0;
+    }
+
+    fseek(file, 0, SEEK_END);
+    int size = ftell(file);
+    fclose(file);
+
+    if (size < 0x800000) {
+        ps1_mcd_attach(iris->ps2->sio2, slot+2, path);
+
+        iris->mcd_slot_type[slot] = 2;
+
+        return 1;
+    }
+
+    mcd_attach(iris->ps2->sio2, slot+2, path);
+
+    iris->mcd_slot_type[slot] = 1;
+
+    return 1;
+}
+
+void detach_memory_card(iris::instance* iris, int slot) {
+    iris->mcd_slot_type[slot] = 0;
+
+    ps2_sio2_detach_device(iris->ps2->sio2, slot+2);
+}
+
 }
