27854f956bd789fb5c64cacdbbcb37f0af04d668
allkern <lisandroaalarcon@gmail.com>
2025-12-19 08:35:50 -0300

    hle: Improve SYSMEM format parsing

--- src/iop/hle/sysmem.c
+++ src/iop/hle/sysmem.c
@@ -10,6 +10,9 @@ uint32_t fetch_next_param(struct iop_state* iop) {
 }
 
 int sysmem_kprintf(struct iop_state* iop) {
+    if (!iop->sm_putchar)
+        return 0;
+
     int ptr = iop->r[4];
 
     reg_index = 5;
@@ -19,6 +22,11 @@ int sysmem_kprintf(struct iop_state* iop) {
     while (c != 0) {
         switch (c) {
             case '%': {
+                int zero_pad = 0;
+                int digits = 0;
+
+                parse:
+
                 c = iop_read8(iop, ptr++);
 
                 switch (c) {
@@ -39,10 +47,41 @@ int sysmem_kprintf(struct iop_state* iop) {
                         }
                     } break;
 
-                    case 'd': {
-                        int val = fetch_next_param(iop);
+                    case '0': {
+                        zero_pad = 1;
+
+                        goto parse;
+                    } break;
+
+                    case '1': case '2': case '3': case '4': 
+                    case '5': case '6': case '7': case '8':
+                    case '9': {
+                        digits = (digits * 10) + (c - '0');
+
+                        goto parse;
+                    } break;
+
+                    case 'd': case 'u': case 'i': case 'x': {
+                        uint32_t val = fetch_next_param(iop);
+
+                        char fmt_buf[8];
+                        char* fmt = fmt_buf;
+
+                        *fmt++ = '%';
+
+                        if (zero_pad) {
+                            *fmt++ = '0';
+                        }
+
+                        if (digits) {
+                            fmt += sprintf(fmt, "%d", digits);
+                        }
+
+                        *fmt++ = c;
+                        *fmt = '\0';
+
                         char buf[16];
-                        sprintf(buf, "%d", val);
+                        sprintf(buf, fmt_buf, val);
 
                         for (char* p = buf; *p != 0; p++) {
                             SM_PUTCHAR(*p);
